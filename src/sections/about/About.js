92c6ab4c19dcf0d4de4962d68d4117b5600925e8
JNematov <jakhongirnem@gmail.com>
2025-01-23 15:08:10 -0600

    styling changes

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -1,9 +1,31 @@
-// import './styles.css';
+import "../../styles.css";
 
 const About = () => {
   return (
     <section className="about">
-      <h1 className="description">
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginLeft: "-20%",
+          fontFamily: "varela round",
+          color: "antiquewhite",
+          fontSize: "200%",
+        }}
+      >
+        About Me
+      </h1>
+      <h2
+        style={{
+          fontFamily: "varela round",
+          color: "antiquewhite",
+          marginBottom: "2%",
+        }}
+      >
+        A bit about me
+      </h2>
+
+      <h3 className="description">
         My name is Paribesh Upreti and I'm an aspiring developer. <br></br>I
         graduated from Fossil Ridge High School in 2021, and I'm currently
         attending UTD as a Computer Science major. <br></br>
@@ -12,7 +34,7 @@ const About = () => {
         and python. <br></br>
         I'm about to graduate this semester, and I'm thrilled to be going into
         the workforce. <br></br>
-      </h1>
+      </h3>
     </section>
   );
 };
